12f0948ed7b57ace906886a90620ae14992fb266
justin <justinmwathi32@gmail.com>
2024-03-20 14:16:05 +0300

    first-class functions lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers=()=>{
+let drivers=['Antonia','Nuru','Amari','Mo'];
+return (drivers.slice(0,2));
+}
+returnFirstTwoDrivers();
+
+const returnLastTwoDrivers=()=>{
+    let drivers=['Antonia','Nuru','Amari','Mo'];
+    return(drivers.slice(2));
+}
+returnLastTwoDrivers();
+
+let selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers];
+
+const createFareMultiplier=function (fareMultiplier){
+    return function (fare){
+        return fareMultiplier * fare;
+    }
+    
+}
+const fareDoubler=createFareMultiplier(2);
+const fareTripler=createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers,selectingDrivers){
+    return(drivers),(selectingDrivers());
+}
+
+
+
+
+
+
+
+
